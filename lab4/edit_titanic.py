118930de85be581f38abccc906785d46abb5784d
tvsmsa <tmmelekhin@gmail.com>
2026-05-11 22:24:00 +0500

    Replace nan

--- lab4/edit_titanic.py
+++ lab4/edit_titanic.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_csv("lab4/Titanic-Dataset.csv")
 df_new = df[['Pclass', 'Sex', 'Age']]
-df_new.to_csv('lab4/titanic_new.csv')
+df_new.to_csv('lab4/titanic_new.csv', index=False)
